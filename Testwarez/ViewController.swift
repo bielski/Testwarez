5d5a10d341266e5a88277ddbb0aa3039bf0a84c8
Ewa Ludwiczak <ewa.ludwiczak@allegro.pl>
2018-11-12 19:48:04 +0100

    Update viewController

--- Testwarez/ViewController.swift
+++ Testwarez/ViewController.swift
@@ -7,34 +7,43 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var button: UIButton!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func viewWillLayoutSubviews() {
-        displayButtonTitleForTraitCollection()
-    }
-
-    // MARK: - Size classes
-    func displayButtonTitleForTraitCollection() {
-        print("Print horizontalSizeClass \(traitCollection.horizontalSizeClass)")
-        if traitCollection.horizontalSizeClass == .regular {
-            button.titleLabel?.text = "Hello Testwarez"
-        } else if traitCollection.horizontalSizeClass == .compact {
-            button.titleLabel?.text = "Hello Testwarez 📱"
-        }
-    }
-
     // MARK: - IBActions
-    @IBAction func buttonTapped(_ sender: Any) {
-        let alert = UIAlertController(title: "Welcome", message: "Nice to see you 👋" , preferredStyle: UIAlertController.Style.alert)
+
+    @IBAction func sayHello(_ sender: Any) {
+        let alert = UIAlertController(title: "Welcome", message: "Nice to see you 😀" , preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "You too!", style: UIAlertAction.Style.default, handler: nil))
-        //self.present(alert, animated: true, completion: nil)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func displayWebsite(_ sender: Any) {
+        var url: URL!
+        let currentDateString = getCurrentDateAsString()
+        print("Current date is \(currentDateString)")
+
+        if currentDateString == "14.11.2018" {
+            url = URL(string: "https://www.testwarez.pl/agenda-day-one/")
+        } else if currentDateString == "15.11.2018" {
+            url = URL(string: "https://www.testwarez.pl/agenda-day-two/")
+        } else {
+            url = URL(string: "https://www.testwarez.pl/")
+        }
+
+        let safariVC: SFSafariViewController = SFSafariViewController(url: url)
+        self.present(safariVC, animated: true, completion: nil)
+    }
+
+    // Private functions
+
+    func getCurrentDateAsString() -> String {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+
+        let result = formatter.string(from: date)
+        return result
     }
 }
-
